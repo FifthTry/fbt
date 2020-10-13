e41daf8e3f21116df916b691a7c9c5e7552ac9af
Sitesh <siteshpattanaik001@gmail.com>
2020-10-14 01:57:56 +0530

    tried to read cmd.toml

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::read_dir;
+use std::fs::{self, read_dir};
 
 fn main() {
     for dirs in read_dir("./tests").unwrap() {
@@ -11,15 +11,26 @@ fn main() {
             let mut cmd_toml_path = None;
             for inner_dirs in read_dir(entry.path()).unwrap() {
                 let entry1 = inner_dirs.unwrap();
-                if entry1.file_type().unwrap().is_dir() && entry1.file_name().to_str().unwrap().contains("input") {
+                if entry1.file_type().unwrap().is_dir()
+                    && entry1.file_name().to_str().unwrap().contains("input")
+                {
                     input_path = Some(entry1.path());
                 }
-                if entry1.file_name().to_str().unwrap().contains("cmd.toml"){
+                if entry1.file_name().to_str().unwrap().contains("cmd.toml") {
                     cmd_toml_path = Some(entry1.path());
                 }
             }
+
+            if input_path == None || cmd_toml_path == None {
+                //Error
+                println!("not a valid test case");
+            }
+
             println!("input : {:?}, cmd.toml {:?}", input_path, cmd_toml_path);
 
+            let contents = fs::read_to_string(cmd_toml_path.unwrap())
+                .expect("Something went wrong reading the file");
+            println!("{:?}", contents);
         }
     }
 }
