47caff3db55c6166d8c9b53999c61b0e1536fd23
Sitesh <siteshpattanaik001@gmail.com>
2020-11-02 23:56:48 +0530

    wip

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,38 @@ use fbt_lib::types::SingleTestResult;
 
 fn main() {
     match fbt_lib::test_all() {
-        Ok(res) => {
-            let mut tests: Vec<SingleTestResult> = vec![];
-            match res.results {
-                Ok(val) => tests = val,
+        Ok(test_result) => {
+            let mut single_results: Vec<SingleTestResult> = vec![];
+            match test_result.results {
+                Ok(result) => single_results = result,
                 _ => {}
             }
-            for test in tests.iter() {
+            for test in single_results.iter() {
                 let id = test.id.clone();
                 match &test.result {
-                    Ok(_) => {
-                        println!(
-                            "Test Name: {}, Success: {}",
-                            id.magenta().to_string(),
-                            "TRUE".green().to_string()
-                        );
+                    Ok(status) => {
+                        if *status {
+                            println!(
+                                "Test: {}, Status: {}, Time: {}",
+                                id.blue().to_string(),
+                                "SKIPPED".magenta().to_string(),
+                                format!("{:?}", &test.duration).yellow().to_string()
+                            );
+                        } else {
+                            println!(
+                                "Test: {}, Status: {}, Time: {}",
+                                id.blue().to_string(),
+                                "SUCCESS".green().to_string(),
+                                format!("{:?}", &test.duration).yellow().to_string()
+                            );
+                        }
                     }
                     Err(_e) => {
                         println!(
-                            "Test Name: {}, Success: {}",
-                            id.magenta().to_string(),
-                            "FALSE".red().to_string()
+                            "Test: {}, Status: {}, Time: {}",
+                            id.blue().to_string(),
+                            "FAILED".red().to_string(),
+                            format!("{:?}", &test.duration).yellow().to_string()
                         );
                     }
                 }
