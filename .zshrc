e3d5f1d597e23870f23d5c7ca3a82947e0d93b29
Amit Upadhyay <upadhyay@gmail.com>
2021-04-30 15:08:17 +0530

    remove python/nix stuff, using pure rust now

--- .zshrc
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-source auto.sh
-test -f ~/.zshrc && source ~/.zshrc
-export HISTFILE=~/.zsh_history
-export PS1="]$PS1"
--- scripts/toml-fmt.sh
+++
@@ -1,3 +0,0 @@
-test -f .cargo/bin/cargo-tomlfmt || cargo install cargo-tomlfmt
-
-find . | grep Cargo.toml | grep -v .cargo | grep -v target-nix | grep -v venv | xargs -n 1 cargo tomlfmt --path
